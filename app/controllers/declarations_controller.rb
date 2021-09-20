82728b7ae1827cfe39dfefd921d9169cd511dd5e
ohno <ddeeppaa.gogo@gmail.com>
2021-09-21 00:22:33 +0900

    Add: 宣言一覧を作成

--- app/controllers/declarations_controller.rb
+++ app/controllers/declarations_controller.rb
@@ -4,5 +4,18 @@ class DeclarationsController < ApplicationController
   end
 
   def index
+    @declarations = Declaration.all
+  end
+
+  def create
+    declaration = Declaration.new(declaration_params)
+    declaration.save!
+    redirect_to declarations_path, notice: "宣言しました！"
+  end
+
+  private
+
+  def declaration_params
+    params.require(:declaration).permit(:nickname, :declaration, :comment)
   end
 end
